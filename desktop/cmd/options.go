4236e0622d24907fc02ae9d73c2c0f4b355b1952
Daniel Newton <djpnewton@gmail.com>
2019-07-27 18:32:47 -0500

    add desktop support for copying the wallet address

--- desktop/cmd/options.go
+++ desktop/cmd/options.go
@@ -3,12 +3,12 @@ package main
 import (
 	"github.com/go-flutter-desktop/go-flutter"
     "github.com/go-flutter-desktop/plugins/url_launcher"
-
-
+    "github.com/djpnewton/plugins/clipboard_manager"
 )
 
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(800, 600),
 
     flutter.AddPlugin(&url_launcher.UrlLauncherPlugin{}),
+    flutter.AddPlugin(&clipboard_manager.ClipboardManagerPlugin{}),
 }
--- desktop/go.mod
+++ desktop/go.mod
@@ -3,6 +3,7 @@ module github.com/djpnewton/zap_app/desktop
 go 1.12
 
 require (
+	github.com/djpnewton/plugins/clipboard_manager v0.0.0-20190727231111-9f469e0f8a92
 	github.com/go-flutter-desktop/go-flutter v0.27.0
 	github.com/go-flutter-desktop/plugins/url_launcher v0.1.0
 	github.com/pkg/errors v0.8.1
