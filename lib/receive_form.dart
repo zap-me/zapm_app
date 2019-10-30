ae6147488e505cde9dbf5ac9b034ad0a23205182
Daniel Newton <djpnewton@gmail.com>
2019-10-30 18:07:25 +1300

    -kill old makeUri requests when making a new one -kill old makeUri requests when exiting the receive form

--- lib/receive_form.dart
+++ lib/receive_form.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:decimal/decimal.dart';
 
@@ -24,41 +25,48 @@ class ReceiveFormState extends State<ReceiveForm> {
   final _uriController = new TextEditingController();
   String _uri;
   String _amountType = "ZAP";
+  StreamSubscription<String> _uriSub;
 
-  Future makeUri() async {
+  Future<String> makeUri() async {
     var amount = Decimal.fromInt(0);
     try {
       amount = Decimal.parse(_amountController.text);
     }
     catch (e) {}
     if (_amountType == "NZD") {
-      amount = await equivalentZapForNzd(amount, Side.sell);
+      try {
+        amount = await equivalentZapForNzd(amount, Side.sell);
+      }
+      catch (e) {
+        return e.toString();
+      }
     }
     return LibZap.paymentUriDec(widget._testnet, widget._address, amount);
   }
 
-  Future updateUriUi() async {
+  void updateUriUi() {
     setState(() {
       _uri = "...";
       _uriController.text = _uri;
     });
-    String uri;
-    try {
-      uri = await makeUri();
-    }
-    catch (e) {
-      uri = e.toString();
-    }
-    setState(()
-    {
-      _uri = uri;
-      _uriController.text = _uri;
+    
+    _uriSub?.cancel();
+    _uriSub = makeUri().asStream().listen((uri) {
+      setState(() {
+        _uri = uri;
+        _uriController.text = uri;
+      });
     });
   }
 
   void onAmountChanged() {
     updateUriUi();
   }
+
+  Future<bool> canLeave() {
+    _uriSub?.cancel();
+    return Future<bool>.value(true); 
+}
 
   ReceiveFormState() : super() {
     _amountController.addListener(onAmountChanged);
@@ -73,57 +81,60 @@ class ReceiveFormState extends State<ReceiveForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: QrWidget(_uri)
-          ),
-          new TextFormField(
-            controller: _uriController,
-            enabled: false,
-            decoration: new InputDecoration(labelText: 'Receive URI'),
-            maxLines: 5,
-          ),
-          new Stack(alignment: const Alignment(1.0, 1.0), children: <Widget>[
-            new TextFormField(
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(labelText: 'Amount'),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter a value';
-                }
-                final dv = Decimal.parse(value);
-                if (dv <= Decimal.fromInt(0)) {
-                  return 'Please enter a value greater then zero';
-                }
-                return null;
-              },
+    return WillPopScope(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: QrWidget(_uri)
             ),
-            new FlatButton(
-                onPressed: () {
-                  setState(() {
-                    if (_amountType == "ZAP")
-                      _amountType = "NZD";
-                    else
-                      _amountType = "ZAP";                
-                  });
-                  updateUriUi();
+            new TextFormField(
+              controller: _uriController,
+              enabled: false,
+              decoration: new InputDecoration(labelText: 'Receive URI'),
+              maxLines: 5,
+            ),
+            new Stack(alignment: const Alignment(1.0, 1.0), children: <Widget>[
+              new TextFormField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(labelText: 'Amount'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a value';
+                  }
+                  final dv = Decimal.parse(value);
+                  if (dv <= Decimal.fromInt(0)) {
+                    return 'Please enter a value greater then zero';
+                  }
+                  return null;
                 },
-                child: new Text(_amountType))
-          ]),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton.icon(
-                onPressed: widget.onClosed,
-                icon: Icon(Icons.close),
-                label: Text('Close')),
-          ),
-        ],
+              ),
+              new FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_amountType == "ZAP")
+                        _amountType = "NZD";
+                      else
+                        _amountType = "ZAP";                
+                    });
+                    updateUriUi();
+                  },
+                  child: new Text(_amountType))
+            ]),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton.icon(
+                  onPressed: widget.onClosed,
+                  icon: Icon(Icons.close),
+                  label: Text('Close')),
+            ),
+          ],
+        ),
       ),
+      onWillPop: canLeave,
     );
   }
 }
