cd7533381768d3884c11c872546f6f055cdb62ed
Daniel Newton <djpnewton@gmail.com>
2020-03-06 13:23:52 +1300

    add libzap.dylib to xcode project

--- lib/dylib_utils.dart
+++ lib/dylib_utils.dart
@@ -9,12 +9,15 @@ String _platformPath(String name, {String path}) {
   if (path == null) path = "";
   if (Platform.isLinux || Platform.isAndroid)
     return path + "lib" + name + ".so";
-  if (Platform.isMacOS) return path + "lib" + name + ".dylib";
-  if (Platform.isWindows) return path + "lib" + name + ".dll";
+  if (Platform.isMacOS || Platform.isIOS)
+    return path + "lib" + name + ".dylib";
+  if (Platform.isWindows)
+    return path + "lib" + name + ".dll";
   throw Exception("Platform not implemented");
 }
 
 ffi.DynamicLibrary dlopenPlatformSpecific(String name, {String path}) {
+  //TODO: use DynamicLibrary.process() on IOS? (https://flutter.dev/docs/development/platform-integration/c-interop#ios-and-macos)
   String fullPath = _platformPath(name, path: path);
   return ffi.DynamicLibrary.open(fullPath);
 }
