e059cf60795aaf351b471861fc7f7e593ea0c431
Daniel Newton <djpnewton@gmail.com>
2021-02-02 19:55:24 +1300

    branding for frankie points

--- lib/config.dart
+++ lib/config.dart
@@ -11,17 +11,17 @@ enum TokenType {
 // the default testnet value
 const TestnetDefault = false;
 // the app title
-const AppTitle = 'Whitelabel Demo';
+const AppTitle = 'Frankies';
 // all instances where the asset short name has the first letter capitalized
-const AssetShortName = 'Paz';
+const AssetShortName = 'FP';
 // all instances where the asset short name is lower case
-const AssetShortNameLower = 'paz';
+const AssetShortNameLower = 'FP';
 // all instances where the asset short name is upper case
-const AssetShortNameUpper = 'PAZ';
+const AssetShortNameUpper = 'FP';
 // the 'zap' icon on the header of the home page
 const AssetHeaderIconPng = 'assets/icon.png';
 // the lightning icon on the home page balance widget
-const AssetBalanceIconSvg = 'assets/paz.svg';
+const AssetBalanceIconSvg = 'assets/fp.svg';
 // enable/disable reward UI/feature
 const UseReward = false;
 // enable/disable settlement UI/feature
@@ -29,13 +29,13 @@ const UseSettlement = false;
 // enable/disable merchant api (including all references/conversions to NZD)
 const UseMerchantApi = false;
 // enable/disable a webview homepage (null = disabled)
-const String WebviewURL = null;
+const String WebviewURL = 'https://sites.google.com/view/frankiecms/home';
 
 // are we using the waves network or a paydb server
 const TokenType AppTokenType = TokenType.Waves;
 
 // Waves settings
-// set these two to use a different mainnet/testnet asset id pair then the default zap asset ids.
+// set these two to use a different mainnet/testnet asset id pair then the defualt zap asset ids.
 const String AssetIdMainnet = null;
 const String AssetIdTestnet = null;
 // set these two to use a different mainnet/testnet node url pair then the default urls.
@@ -55,11 +55,11 @@ const String CentrapayApiKey = 'PvfgoXxDv2FCzsgVsk1izM41e5tWd7C8AGuThG9M1';
 
 void initConfig() {
   overrideTheme(
-    zapWhite: Colors.lightBlue[50],
-    zapYellow: Colors.teal[100],
+    zapWhite: Colors.white,
+    zapYellow: Color.fromARGB(255, 237, 29, 38),
     zapWarning: Colors.yellow,
     zapWarningLight: Colors.yellow[100],
-    zapBlue: Colors.pink[200],
-    zapGreen: Colors.blueGrey[300],
-    zapTextThemer: GoogleFonts.sansitaTextTheme);
+    zapBlue: Color.fromARGB(255, 23, 125, 57),
+    zapGreen: Colors.yellow,
+    zapTextThemer: GoogleFonts.robotoTextTheme);
 }
