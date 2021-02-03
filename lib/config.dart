96080103b21c19a70dbf208ba29db1a9f326d4ed
Daniel Newton <djpnewton@gmail.com>
2021-02-03 15:59:58 +1300

    change "frankies" styling to "M Token"

--- lib/config.dart
+++ lib/config.dart
@@ -11,17 +11,17 @@ enum TokenType {
 // the default testnet value
 const TestnetDefault = false;
 // the app title
-const AppTitle = 'Frankies';
+const AppTitle = 'M Token';
 // all instances where the asset short name has the first letter capitalized
-const AssetShortName = 'FP';
+const AssetShortName = 'MTOK';
 // all instances where the asset short name is lower case
-const AssetShortNameLower = 'FP';
+const AssetShortNameLower = 'mtok';
 // all instances where the asset short name is upper case
-const AssetShortNameUpper = 'FP';
+const AssetShortNameUpper = 'MTOK';
 // the 'zap' icon on the header of the home page
 const AssetHeaderIconPng = 'assets/icon.png';
 // the lightning icon on the home page balance widget
-const AssetBalanceIconSvg = 'assets/fp.svg';
+const AssetBalanceIconSvg = 'assets/m.svg';
 // enable/disable reward UI/feature
 const UseReward = false;
 // enable/disable settlement UI/feature
@@ -39,7 +39,7 @@ const TokenType AppTokenType = TokenType.Waves;
 const String AssetIdMainnet = null;
 const String AssetIdTestnet = null;
 // set these two to use a different mainnet/testnet node url pair then the default urls.
-const String NodeUrlMainnet = null;
+const String NodeUrlMainnet = 'https://node.zap.me';
 const String NodeUrlTestnet = null;
 
 // PayDB settings
@@ -56,10 +56,10 @@ const String CentrapayApiKey = 'PvfgoXxDv2FCzsgVsk1izM41e5tWd7C8AGuThG9M1';
 void initConfig() {
   overrideTheme(
     zapWhite: Colors.white,
-    zapYellow: Color.fromARGB(255, 237, 29, 38),
+    zapYellow: Color.fromARGB(255, 255, 65, 93),
     zapWarning: Colors.yellow,
     zapWarningLight: Colors.yellow[100],
-    zapBlue: Color.fromARGB(255, 23, 125, 57),
-    zapGreen: Colors.yellow,
+    zapBlue: Color.fromARGB(255, 7, 65, 173),
+    zapGreen: Color.fromARGB(255, 123, 216, 58),
     zapTextThemer: GoogleFonts.robotoTextTheme);
 }
