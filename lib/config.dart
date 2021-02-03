ad4553e20d23e04ee6a6a25f6e9e40e8b4709636
Daniel Newton <djpnewton@gmail.com>
2021-02-03 15:59:58 +1300

    change "frankies" styling to "M Token"

--- lib/config.dart
+++ lib/config.dart
@@ -6,17 +6,17 @@ import 'package:zapdart/colors.dart';
 // the default testnet value
 const TestnetDefault = false;
 // the app title
-const AppTitle = 'Frankies';
+const AppTitle = 'M Token';
 // all instances where the asset short name has the first letter capitalized
-const AssetShortName = 'FP';
+const AssetShortName = 'MTOK';
 // all instances where the asset short name is lower case
-const AssetShortNameLower = 'FP';
+const AssetShortNameLower = 'mtok';
 // all instances where the asset short name is upper case
-const AssetShortNameUpper = 'FP';
+const AssetShortNameUpper = 'MTOK';
 // the 'zap' icon on the header of the home page
 const AssetHeaderIconPng = 'assets/icon.png';
 // the lightning icon on the home page balance widget
-const AssetBalanceIconSvg = 'assets/fp.svg';
+const AssetBalanceIconSvg = 'assets/m.svg';
 // enable/disable reward UI/feature
 const UseReward = false;
 // enable/disable settlement UI/feature
@@ -29,7 +29,7 @@ const String WebviewURL = 'https://sites.google.com/view/frankiecms/home';
 const String AssetIdMainnet = null;
 const String AssetIdTestnet = null;
 // set these two to use a different mainnet/testnet node url pair then the default urls.
-const String NodeUrlMainnet = null;
+const String NodeUrlMainnet = 'https://node.zap.me';
 const String NodeUrlTestnet = null;
 // If set the app will try to register with the premio stage server for push notifications
 const String PremioStageIndexUrl = null;
@@ -38,10 +38,10 @@ const String PremioStageName = null;
 void initConfig() {
   overrideTheme(
     zapWhite: Colors.white,
-    zapYellow: Color.fromARGB(255, 237, 29, 38),
+    zapYellow: Color.fromARGB(255, 255, 65, 93),
     zapWarning: Colors.yellow,
     zapWarningLight: Colors.yellow[100],
-    zapBlue: Color.fromARGB(255, 23, 125, 57),
-    zapGreen: Colors.yellow,
+    zapBlue: Color.fromARGB(255, 7, 65, 173),
+    zapGreen: Color.fromARGB(255, 123, 216, 58),
     zapTextThemer: GoogleFonts.robotoTextTheme);
 }
