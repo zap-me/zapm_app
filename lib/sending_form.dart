18a5db2cecc1d10b9ed506f03dc4fb430e8484f1
Daniel Newton <djpnewton@gmail.com>
2021-08-04 11:26:56 +1200

    update paydb tx send error messages

--- lib/sending_form.dart
+++ lib/sending_form.dart
@@ -101,15 +101,24 @@ class PayDbSendingForm extends StatefulWidget {
 
 class PayDbSendingFormState extends State<PayDbSendingForm> {
   bool _sending = true;
-  PayDbTx? _tx;
+  PayDbTxResult? _txResult;
 
   void send() async {
     var result = await paydbTransactionCreate(
         ActionTransfer, widget._recipient, widget._amount, widget._attachment);
     setState(() {
       _sending = false;
-      _tx = result.tx;
+      _txResult = result;
     });
+  }
+
+  Widget txErrorMsg(PayDbError? err) {
+    if (err == PayDbError.Auth)
+      return Text(capFirst(
+          'recipient user does not exist, please check that the email is correct'));
+    if (err == PayDbError.Network)
+      return Text(capFirst('transaction failed (network unavailable)'));
+    return Text(capFirst('transaction failed :('));
   }
 
   @override
@@ -139,21 +148,26 @@ class PayDbSendingFormState extends State<PayDbSendingForm> {
               child: Text(capFirst('creating transaction...')),
             ),
             Visibility(
-              visible: !_sending && _tx != null,
-              child: Text(capFirst('transaction complete (${_tx?.token})'),
+              visible: !_sending && _txResult?.tx != null,
+              child: Text(
+                  capFirst('transaction complete (${_txResult?.tx?.token})'),
                   style: TextStyle(color: ZapBlue)),
             ),
             Visibility(
-              visible: !_sending && _tx == null,
-              child: Text(capFirst('transaction failed :(')),
+              visible: !_sending && _txResult?.tx == null,
+              child: txErrorMsg(_txResult?.error),
             ),
             Visibility(
                 visible: !_sending,
                 child: Container(padding: const EdgeInsets.only(top: 20.0))),
             Visibility(
                 visible: !_sending,
-                child: RoundedButton(() => Navigator.pop(context, _tx), ZapBlue,
-                    ZapWhite, null, capFirst('close'),
+                child: RoundedButton(
+                    () => Navigator.pop(context, _txResult?.tx),
+                    ZapBlue,
+                    ZapWhite,
+                    null,
+                    capFirst('close'),
                     borderColor: ZapBlue)),
           ],
         ),
