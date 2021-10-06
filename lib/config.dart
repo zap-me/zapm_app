7c48cfc231e2503cfe9f0b7a2d75e7aeebefb67b
Daniel Newton <djpnewton@gmail.com>
2021-10-07 10:12:01 +1300

    - add 'https://t.sharethis.com' to webview whitelist - bump version (1.3.18+61)

--- lib/config.dart
+++ lib/config.dart
@@ -32,7 +32,7 @@ const UseSettlement = false;
 const UseMerchantApi = false;
 // enable/disable a webview homepage (null = disabled)
 const String? WebviewURL = 'https://www.frankietoken.com';
-const WebviewURLWhitelist = ['https://sharethis.com', 'http://sharethis.com'];
+const WebviewURLWhitelist = ['https://sharethis.com', 'http://sharethis.com', 'https://t.sharethis.com', 'http://t.sharethis.com'];
 // enable/disable the centered ZAP button
 const ZapButton = true;
 const ZapButtonIcon = FlutterIcons.plus_faw5s;
